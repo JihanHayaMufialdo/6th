2143e761cda7a843d1d7061ac6bf032b5abfbe02
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-23 21:18:22 +0700

    Add files via upload

---
+++ FUNGSI DENGAN PARAMETER.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int coba(int a,int b)
+{
+    a=10;
+    b=20;
+    cout<<a<<", "<<b<<endl;
+}
+
+int main()
+{
+    int a, b;
+    
+    a=1;
+    b=2;
+    
+    cout<<a<<", "<<b<<endl;
+    
+    coba (a,b); 
+    
+    cout<<a<<", "<<b<<endl;
+}
