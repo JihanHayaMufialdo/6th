118cd0001f7c2f3b507cdaac51a1536dabc60373
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-24 20:31:03 +0700

    Update Luas & Keliling Bangun Datar.cpp

--- Luas & Keliling Bangun Datar.cpp
+++ Luas & Keliling Bangun Datar.cpp
@@ -8,9 +8,13 @@ void persegi(){
 	cout << "sisi (m) : ";
 	cin  >> s; 
 	
+	if(s!=0){
 	cout << "Keliling : " << 4*s << " m\n";
 	cout << "Luas\t : " << s*s << " m^2";
+	}
 }
+
+
 
 void lingkaran(){
 	float r;
