aa867891fe396ecd28cc89c022b679609e1b949a
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-24 20:33:47 +0700

    Update Luas & Keliling Bangun Datar.cpp

--- Luas & Keliling Bangun Datar.cpp
+++ Luas & Keliling Bangun Datar.cpp
@@ -51,7 +51,27 @@ void segitiga(){
 	}
 }
 
-
+void trapesium(){
+	float a,b,t,s;
+	
+	cout << "\n------- TRAPESIUM -------\n\n";
+	cout << "sisi atas (m)    : ";
+	cin  >> a; 
+	cout << "sisi bawah (m)   : ";
+	cin  >> b;
+	cout << "tinggi (m)       : ";
+	cin  >> t;
+	cout << "sisi samping (m) : ";
+	cin  >> s;
+	
+	if(s!=0 && a!=0 && b!=0){
+	cout << "\nKeliling : " << 2*s+a+b << " m\n";
+	}
+	
+	if(a!=0 && b!=0 && t!=0){
+	cout << "Luas\t : " << ((a+b)*t)/2 << " m^2";
+	}
+}
 
 void lingkaran(){
 	float r;
