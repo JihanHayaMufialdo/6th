83aa762f9427863ff80c9cfba8abfb70b3678863
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-11-24 20:36:38 +0700

    Update Luas & Keliling Bangun Datar.cpp

--- Luas & Keliling Bangun Datar.cpp
+++ Luas & Keliling Bangun Datar.cpp
@@ -109,6 +109,25 @@ void layanglayang(){
 	}
 }
 
+void jajargenjang(){
+	float a,t,s;
+	
+	cout << "\n----- JAJAR GENJANG -----\n\n";
+	cout << "alas (m)         : ";
+	cin  >> a; 
+	cout << "tinggi (m)       : ";
+	cin  >> t;
+	cout << "sisi samping (m) : ";
+	cin  >> s;
+	
+	if(a!=0 && t!=0){ 
+	cout << "\nLuas\t : " << a*t << " m^2";
+	}
+	
+	if(a!=0 && s!=0){
+	cout << "\nKeliling : " << 2*(a+s)<< " m\n";
+	}
+}
 
 
 
