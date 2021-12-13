f357dea6519a371b9cca71c56591eac068a39e1d
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2021-12-13 22:14:45 +0700

    Update fungsi dengan parameter.cpp

--- fungsi dengan parameter.cpp
+++ fungsi dengan parameter.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int coba(int a,int b)
 {
-   
+    a=10;
+    b=20;
+    cout << a << ", " << b <<endl;
 }
 
 int main()
 {
     int a, b;
     
-    a=1;
-    b=2;
+    
     
     cout << a << ", " << b <<endl;
     
